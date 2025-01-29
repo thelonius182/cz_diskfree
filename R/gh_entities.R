4c54b44e546df4d77a7376db5386c748edda81a4
thelonius182 <lcavdakker@gmail.com>
2025-01-29 21:20:56 +0100

    backup progress 2025-01-29 2120

--- R/gh_entities.R
+++ R/gh_entities.R
@@ -37,6 +37,15 @@ and post_date > '2024-01-01'
 order by 1;"
 wp_translations <- dbGetQuery(wpdb, sql_stm)
 
+swf_translation <- wp_translations |>
+  mutate(post_id_nl = str_extract(post_translations,
+                                  pattern = '"nl";i:(\\d{6,});',
+                                  group = 1) |> as.integer(),
+         post_id_en = str_extract(post_translations,
+                                  pattern = '"en";i:(\\d{6,});',
+                                  group = 1) |> as.integer()) |>
+  select(-post_translations)
+
 sql_stm <- "select object_id as post_id, t1.slug as lang
 from wp_term_relationships r1
 join wp_term_taxonomy x1 on x1.term_taxonomy_id = r1.term_taxonomy_id
@@ -60,3 +69,36 @@ where x1.taxonomy = 'programma_genre'
 order by 1;
 "
 wp_genre <- dbGetQuery(wpdb, sql_stm)
+wrk_genre_err <- wp_genre |> group_by(post_id) |> summarize(n = n()) |> filter(n > 2)
+
+swf_title <- wp_genre |> anti_join(wrk_genre_err, by = join_by(post_id)) |>
+  mutate(title_id = term_id,
+         title_slug = str_extract(genre, "(.*)__.*", group = 1),
+         genre_slug = str_extract(genre, "__(.*)-(nl|en)$", group = 1),
+         lang = str_extract(genre, "__.*-(..)$", group = 1)) |>
+  select(-term_id, -genre) |> arrange(post_id, genre_slug) |>
+  group_by(post_id) %>%
+  summarize(
+    title_id = first(title_id),
+    title_slug = first(title_slug),
+    genre_slug = paste(genre_slug, collapse = "-"),
+    lang = first(lang),
+    .groups = "drop"
+  )
+
+sql_stm <- "select object_id as post_id, t1.term_id, t1.slug as editor
+from wp_term_relationships r1
+     join wp_term_taxonomy x1 on x1.term_taxonomy_id = r1.term_taxonomy_id
+     join wp_terms t1 on t1.term_id = x1.term_id
+     join wp_posts p1 on p1.ID = r1.object_id
+where x1.taxonomy = 'programma_maker'
+  and post_type regexp '^programma'
+  and post_date > '2024-01-01'
+order by 1;
+"
+wp_editor <- dbGetQuery(wpdb, sql_stm)
+wrk_editor_err <- wp_editor |> group_by(post_id) |> summarize(n = n()) |> filter(n > 4)
+swf_editor <- wp_editor |> anti_join(wrk_editor_err, by = join_by(post_id)) |>
+  mutate(editor_id = term_id,
+         editor_slug = str_extract(editor, "(.*)-(nl|en)$", group = 1),
+         lang = str_extract(editor, ".*-(..)$", group = 1))
